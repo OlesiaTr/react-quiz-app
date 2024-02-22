e0ac924e1ad943178ffbac82d4e5d75b9b071a0d
OlesiaTr <104642736+OlesiaTr@users.noreply.github.com>
2024-02-23 00:49:07 +0200

    + generic-data

---
+++ src/types/generic-data.ts
@@ -0,0 +1,22 @@
+export interface GenericData {
+  loader: {
+    'sub-title': string;
+  };
+  email: {
+    title: string;
+    'sub-title': string;
+    placeholder: string;
+    'empty-error': string;
+    'incorrect-error': string;
+    agreement: string;
+  };
+  success: {
+    title: string;
+    'sub-title': string;
+    download: string;
+  };
+  button: {
+    next: string;
+    retake: string;
+  };
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,3 @@
+export { type GenericData } from './generic-data.ts';
 export { type Option } from './options.ts';
 export { type QuestionData } from './question-data.ts';
