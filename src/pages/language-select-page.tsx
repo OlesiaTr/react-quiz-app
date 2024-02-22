0663e985e46741f32aca03539117aa50408dcf92
OlesiaTr <104642736+OlesiaTr@users.noreply.github.com>
2024-02-22 20:02:40 +0200

    + language-select-page

---
+++ src/pages/language-select-page.tsx
@@ -0,0 +1,47 @@
+import { useLocation } from 'react-router-dom';
+
+import { ProgressBar } from '../components/progress-bar';
+import { TOTAL_AMOUNT_OF_QUESTIONS } from '../constants';
+import { Title } from '../components/title';
+import { SubTitle } from '../components/sub-title';
+import { LanguageOption } from '../components/options';
+import { useLocalization } from '../hooks';
+import { getPageNumberFromPath } from '../helpers';
+
+const LanguageSelectPage = () => {
+  const { pathname } = useLocation();
+  const page = getPageNumberFromPath(pathname);
+  const { selectedTranslation, changeLanguageAndNavigate } = useLocalization();
+
+  const handleLanguageSelect = (language: string) => {
+    changeLanguageAndNavigate(language, page);
+  };
+
+  if (selectedTranslation === null) return <h1>Loading...</h1>;
+
+  return (
+    <>
+      <ProgressBar
+        question={Number(page)}
+        totalQuestions={TOTAL_AMOUNT_OF_QUESTIONS}
+        isGoBackBtnShown={false}
+      />
+
+      <section>
+        <Title>{selectedTranslation[0].title}</Title>
+        <SubTitle>{selectedTranslation[0]['sub-title']}</SubTitle>
+        <ul>
+          {selectedTranslation[0].options.map(({ value }) => (
+            <LanguageOption
+              onClick={() => handleLanguageSelect(value)}
+              language={value}
+              key={value}
+            />
+          ))}
+        </ul>
+      </section>
+    </>
+  );
+};
+
+export default LanguageSelectPage;
