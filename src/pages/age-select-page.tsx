ff8e9635ae0927b5ddfbe2f1beeb74a3c711ad4c
OlesiaTr <104642736+OlesiaTr@users.noreply.github.com>
2024-02-22 20:03:08 +0200

    + age-select-page

---
+++ src/pages/age-select-page.tsx
@@ -0,0 +1,44 @@
+import { useLocation, useNavigate } from 'react-router-dom';
+import { ProgressBar } from '../components/progress-bar';
+import { AGE_KEY, TOTAL_AMOUNT_OF_QUESTIONS } from '../constants';
+import { getPageNumberFromPath } from '../helpers';
+import { useLocalization } from '../hooks';
+import { Title } from '../components/title';
+import { AgeOption } from '../components/options';
+
+const AgeSelectPage = () => {
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+  const page = getPageNumberFromPath(pathname);
+  const { selectedTranslation } = useLocalization();
+
+  const handleAgeSelect = (gender: string) => {
+    localStorage.setItem(AGE_KEY, gender);
+    navigate(`/quiz/${Number(page) + 1}`);
+  };
+
+  if (selectedTranslation === null) return <h1>Loading...</h1>;
+
+  return (
+    <>
+      <ProgressBar
+        question={Number(page)}
+        totalQuestions={TOTAL_AMOUNT_OF_QUESTIONS}
+      />
+      <section>
+        <Title>{selectedTranslation[2].title}</Title>
+        <ul>
+          {selectedTranslation[2].options.map(({ value }) => (
+            <AgeOption
+              onClick={() => handleAgeSelect(value)}
+              age={value}
+              key={value}
+            />
+          ))}
+        </ul>
+      </section>
+    </>
+  );
+};
+
+export default AgeSelectPage;
