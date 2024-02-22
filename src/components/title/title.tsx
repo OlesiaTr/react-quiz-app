108c8425d34bd3bd78a5407cc5e61c9fdc5aeedc
OlesiaTr <104642736+OlesiaTr@users.noreply.github.com>
2024-02-23 00:49:52 +0200

    * title

--- src/components/title/title.tsx
+++ src/components/title/title.tsx
@@ -3,10 +3,19 @@ import { Text } from './title.styled';
 
 export interface Props {
   children: ReactNode;
+  htmlContent?: string;
 }
 
-const Title: FC<Props> = ({ children }) => {
-  return <Text>{children}</Text>;
+const Title: FC<Props> = ({ children, htmlContent }) => {
+  return (
+    <>
+      {htmlContent ? (
+        <Text dangerouslySetInnerHTML={{ __html: htmlContent }} />
+      ) : (
+        <Text>{children}</Text>
+      )}
+    </>
+  );
 };
 
 export default Title;
