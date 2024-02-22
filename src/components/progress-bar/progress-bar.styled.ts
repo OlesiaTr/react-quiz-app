b15cfb4ab9b8cf3bab7269b85cbd944e23a0bf32
OlesiaTr <104642736+OlesiaTr@users.noreply.github.com>
2024-02-22 19:59:57 +0200

    + progress-bar

---
+++ src/components/progress-bar/progress-bar.styled.ts
@@ -0,0 +1,71 @@
+import { styled } from 'styled-components';
+import { theme } from '../../styles';
+
+export const Section = styled.section`
+  justify-content: space-between;
+  align-items: center;
+  color: white;
+  flex-direction: column;
+  margin-bottom: 30px;
+  width: 100%;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  margin-bottom: 8px;
+  justify-content: space-between;
+  align-items: center;
+  height: 40px;
+`;
+
+export const GoBackBtn = styled.button<{ $isGoBackBtnShown: boolean }>`
+  background: none;
+  border: none;
+  cursor: ${({ $isGoBackBtnShown }) =>
+    $isGoBackBtnShown ? 'pointer' : 'default'};
+  transition: ${theme.transition.main};
+  opacity: ${({ $isGoBackBtnShown }) => ($isGoBackBtnShown ? 1 : 0)};
+  animation: ${({ $isGoBackBtnShown }) =>
+      $isGoBackBtnShown ? 'fadeIn' : 'none'}
+    0.5s ease;
+
+  &:active svg path {
+    stroke: ${theme.colors.secondaryPink};
+  }
+`;
+
+export const QuestionContainer = styled.p`
+  margin: 0 auto;
+
+  font-size: ${theme.fontSizes.xm};
+  font-weight: ${theme.fontWeights['extra-bold']};
+  line-height: 111%;
+
+  @media screen and (${theme.media.medium}) {
+    font-size: ${theme.fontSizes.l};
+  }
+`;
+
+export const CurrentQuestion = styled.span`
+  color: ${theme.colors.secondaryPink};
+`;
+
+export const Bar = styled.div`
+  width: 100%;
+  height: 4px;
+  background-color: ${theme.colors.white};
+  border-radius: 5px;
+  overflow: hidden;
+
+  @media screen and (${theme.media.medium}) {
+    height: 10px;
+  }
+`;
+
+export const ProgressPercent = styled.div<{ $progressPercent: number }>`
+  width: ${props => props.$progressPercent}%;
+  height: 100%;
+  background-color: ${theme.colors.secondaryPink};
+  transition: width 1s;
+  border-radius: 10px;
+`;
---
+++ src/components/progress-bar/progress-bar.tsx
@@ -0,0 +1,60 @@
+import { FC, useEffect, useState } from 'react';
+import { ArrowLeftIcon } from '../../assets/icons';
+import {
+  Bar,
+  Container,
+  CurrentQuestion,
+  GoBackBtn,
+  ProgressPercent,
+  QuestionContainer,
+  Section,
+} from './progress-bar.styled';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { getPageNumberFromPath } from '../../helpers';
+
+export interface Props {
+  question: number;
+  totalQuestions: number;
+  isGoBackBtnShown?: boolean;
+}
+
+const ProgressBar: FC<Props> = ({
+  question,
+  totalQuestions,
+  isGoBackBtnShown = true,
+}) => {
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+  const page = getPageNumberFromPath(pathname);
+  const [progressPercent, setProgressPercent] = useState(0);
+
+  useEffect(() => {
+    const newProgressPercent = (question / totalQuestions) * 100;
+    setProgressPercent(newProgressPercent);
+  }, [question, totalQuestions]);
+
+  const handleBtnClick = () => navigate(`/quiz/${Number(page) - 1}`);
+
+  return (
+    <Section>
+      <Container>
+        <GoBackBtn
+          $isGoBackBtnShown={isGoBackBtnShown}
+          onClick={handleBtnClick}
+        >
+          <ArrowLeftIcon />
+        </GoBackBtn>
+
+        <QuestionContainer>
+          <CurrentQuestion>{question}</CurrentQuestion>
+          <span>/{totalQuestions}</span>
+        </QuestionContainer>
+      </Container>
+      <Bar>
+        <ProgressPercent $progressPercent={progressPercent} />
+      </Bar>
+    </Section>
+  );
+};
+
+export default ProgressBar;
