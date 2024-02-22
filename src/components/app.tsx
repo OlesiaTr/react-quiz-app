9b1669ebc103a0ddecdb0b4af3b346159acfc953
OlesiaTr <104642736+OlesiaTr@users.noreply.github.com>
2024-02-22 20:03:28 +0200

    * app.tsx

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,8 +1,24 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+
+// components
+import { SharedLayout } from './shared-layout';
+import { lazy } from 'react';
+
+// pages
+const LanguageSelect = lazy(() => import('../pages/language-select-page.tsx'));
+const GenderSelect = lazy(() => import('../pages/gender-select-page.tsx'));
+const AgeSelect = lazy(() => import('../pages/age-select-page.tsx'));
+
 const App = () => {
   return (
-    <>
-      <h1>App Component</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Navigate to="/quiz/1" replace />} />
+        <Route path="quiz/1" element={<LanguageSelect />} />
+        <Route path="quiz/2" element={<GenderSelect />} />
+        <Route path="quiz/3" element={<AgeSelect />} />
+      </Route>
+    </Routes>
   );
 };
 
