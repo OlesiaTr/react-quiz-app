ff569f31030857e14be7c5c84b2b2ce16e21e296
OlesiaTr <104642736+OlesiaTr@users.noreply.github.com>
2024-02-23 11:03:21 +0200

    Updates

---
+++ assets/sub-title-JPg9hapS.js
@@ -0,0 +1,17 @@
+import{b as o,t,j as n}from"./index-CpFTmIrb.js";const i=o.h2`
+  color: ${t.colors.subtitleGray};
+
+  text-align: center;
+  font-family: ${t.fonts.nunito};
+  font-size: ${t.fontSizes.m};
+  font-weight: ${t.fontWeights.normal};
+
+  line-height: 141%;
+
+  margin-bottom: 24px;
+
+  @media screen and (${t.media.medium}) {
+    font-size: ${t.fontSizes.xm};
+    margin-bottom: 28px;
+  }
+`,m=({children:e})=>n.jsx(i,{children:e});export{m as S};
