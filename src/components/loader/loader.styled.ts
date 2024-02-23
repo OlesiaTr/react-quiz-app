a06a6f7136a510ec7b67779bd489ec8b5ce6e4c1
OlesiaTr <104642736+OlesiaTr@users.noreply.github.com>
2024-02-23 12:42:19 +0200

    + loader

---
+++ src/components/loader/loader.styled.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+import { theme } from '../../styles';
+
+export const Container = styled.svg`
+  width: 100%;
+  height: 100%;
+  transform: rotate(-90deg);
+`;
+
+export const Background = styled.circle`
+  fill: none;
+  stroke: ${theme.colors.white};
+  stroke-width: 6;
+`;
+
+export const Circle = styled.circle`
+  fill: none;
+  stroke: ${theme.colors.secondaryPink};
+  stroke-width: 6;
+  stroke-linecap: round;
+  transition: ${theme.transition.main};
+`;
+
+export const Text = styled.text`
+  text-anchor: middle;
+  dominant-baseline: central;
+  font-size: ${theme.fontSizes.m};
+  fill: ${theme.colors.white};
+`;
---
+++ src/components/loader/loader.tsx
@@ -0,0 +1,46 @@
+import { FC, useEffect, useState } from 'react';
+import { Background, Circle, Container, Text } from './loader.styled';
+
+export interface Props {
+  onLoadingDone: () => void;
+}
+
+const Loader: FC<Props> = ({ onLoadingDone }) => {
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setProgress(prevProgress =>
+        prevProgress < 100 ? prevProgress + 1 : 100
+      );
+    }, 50);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    if (progress === 100) {
+      setTimeout(() => {
+        onLoadingDone();
+      }, 100);
+    }
+  }, [onLoadingDone, progress]);
+
+  return (
+    <Container viewBox="0 0 100 100">
+      <Background cx="50" cy="50" r="40" />
+      <Circle
+        cx="50"
+        cy="50"
+        r="40"
+        strokeDasharray={251.2}
+        strokeDashoffset={(251.2 * (100 - progress)) / 100}
+      />
+      <Text x="50" y="50" transform="translate(100) rotate(90)">
+        {progress}%
+      </Text>
+    </Container>
+  );
+};
+
+export default Loader;
