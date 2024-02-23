c62bd5944f054ed55ba76e07913a1b617d498663
OlesiaTr <104642736+OlesiaTr@users.noreply.github.com>
2024-02-23 16:00:35 +0200

    * button

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -4,13 +4,19 @@ import { StyledButton } from './button.styled';
 export interface Props {
   children: ReactNode;
   selectedItems?: string[];
+  isDisabled?: boolean;
   onClick: () => void;
 }
 
-const Button: FC<Props> = ({ children, selectedItems, onClick }) => {
+const Button: FC<Props> = ({
+  children,
+  selectedItems,
+  onClick,
+  isDisabled,
+}) => {
   return (
     <StyledButton
-      disabled={selectedItems && selectedItems.length === 0}
+      disabled={(selectedItems && selectedItems.length === 0) || isDisabled}
       onClick={onClick}
     >
       {children}
